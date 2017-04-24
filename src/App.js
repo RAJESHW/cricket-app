a01dce5dffc49d652f886d1fdac2dd3432653e00
vinu123 <wenothe@gmail.com>
2017-04-24 09:59:51 +0530

    Fixed the error but you need to install CORS extension from the chrome store

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-            this.state = {
-                match : 'no match',
-                result: 'no result'
-            }
+  constructor(props) {
+    super(props)
+    this.state = {
+      match : 'no match',
+      result: 'no result'
     }
-    
-    componentWillMount() {
-        fetch('https://cricscore-api.appspot.com/csa', {
-            mode: 'no-cors'
-        }).then(function (response) {
-            response.json()
-        }).then(function(json) {
-            console.log(json);
-        })
-    }
+  }
 
-    render() {
+  /*
+    INSTALL
+    CORS chrome extension and enable it and then run the server
+    https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
+    Keep the extension disabled for other than the development
+  */
+
+  // componentDidMount is the right lifecycle method for a network call
+  componentDidMount() {
+    fetch('https://cricscore-api.appspot.com/csa')
+    .then((res) => {
+      // You were not returning the JSON to the next then()
+      return res.json()
+    })
+    .then((json) => {
+      console.log(json)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  render() {
     return (
       <div className="App">
         <div>
-            <span>{this.state.match}</span>
-            <span>{this.state.result}</span>
+          <span>{this.state.match}</span>
+          <span>{this.state.result}</span>
         </div>
       </div>
-    );
-    }
+    )
+  }
 }
 
-export default App;
+export default App
