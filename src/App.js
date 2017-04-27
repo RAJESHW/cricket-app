403c6d7cc17052fa9dddd4d8240c8141d200fa7d
Rajesh Wellington <RWellington@HollandAmericaGroup.com>
2017-04-27 19:19:00 +0530

    Added Enhancement 1. Rendering Table 2. Show individual match data in the App

--- src/App.js
+++ src/App.js
@@ -2,46 +2,97 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      match : 'no match',
-      result: 'no result'
+    constructor(props) {
+      super(props)
+      this.state = {
+        match : [],
+        condition : 'ShowTable'
+      }
+      this.showResult = this.showResult.bind(this);
+      this.apiFetchCall = this.apiFetchCall.bind(this);
     }
-  }
+    renderChildren(){
+        let matches = this.state.match;
+        if(matches.length) {
+            return Object.keys(matches)
+                .map((item, index) => {
+                    return (
+                     <tr key={index}>
+                        <td>{index+1}.</td>
+                        <td>{matches[item]['t1']}</td>
+                        <td>{matches[item]['t2']}</td>
+                        <td> <a id={matches[item]['id']} onClick={this.showResult} href="#"> Show Result</a></td>
+                     </tr>
+                    )
+                });
+        } else {
+            return  (<tr>
+                        <td>Loading...</td>
+                     </tr> );
+        }
 
-  /*
-    INSTALL
-    CORS chrome extension and enable it and then run the server
-    https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
-    Keep the extension disabled for other than the development
-  */
+    }
+    individualValue() {
+      let matches = this.state.match;
+      if(matches.length) {
+          return Object.keys(matches)
+              .map((item, index) => {
+                  return (
+                   <tr key={index}>
+                      <td>{matches[item].de}</td>
+                   </tr>
+                  )
+              });
+      }
+    }
+    showResult(e) {
+      e.preventDefault();
+      let match_id = e.target.id;
+      console.log(match_id);
+      fetch('https://cricscore-api.appspot.com/csa?id='+match_id)
+      .then((res) => {
+        // You were not returning the JSON to the next then()
+        return res.json()
+      })
+      .then((json) => {
+        this.setState({
+            match: json,
+            condition: 'ShowResults'
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+    apiFetchCall(e) {
+      fetch('https://cricscore-api.appspot.com/csa')
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        this.setState({
+            match: json
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 
-  // componentDidMount is the right lifecycle method for a network call
-  componentDidMount() {
-    fetch('https://cricscore-api.appspot.com/csa')
-    .then((res) => {
-      // You were not returning the JSON to the next then()
-      return res.json()
-    })
-    .then((json) => {
-      console.log(json)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+    }
+    render() {
 
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <span>{this.state.match}</span>
-          <span>{this.state.result}</span>
+
+      return (
+        <div className="App">
+        <button onClick={this.apiFetchCall}>Show Table</button>
+          <table>
+            <tbody>
+                    { (this.state.condition === 'ShowTable') ? (this.renderChildren()) : (this.individualValue()) }                    }
+            </tbody>
+          </table>
         </div>
-      </div>
-    )
-  }
+      )
+    }
 }
 
 export default App
