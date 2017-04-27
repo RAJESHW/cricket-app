4822cfe7bd6b2418d71f25f9d3af0ceb2e0dd059
vinu123 <wenothe@gmail.com>
2017-04-27 20:13:21 +0530

    Introduced conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,95 +1,56 @@
 import React, { Component } from 'react'
 import './App.css'
 
+/*
+  1. Grab the data and set the state first
+*/
+
 class App extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        match : [],
-        condition : 'ShowTable'
-      }
-      this.showResult = this.showResult.bind(this);
-      this.apiFetchCall = this.apiFetchCall.bind(this);
-    }
-    renderChildren(){
-        let matches = this.state.match;
-        if(matches.length) {
-            return Object.keys(matches)
-                .map((item, index) => {
-                    return (
-                     <tr key={index}>
-                        <td>{index+1}.</td>
-                        <td>{matches[item]['t1']}</td>
-                        <td>{matches[item]['t2']}</td>
-                        <td> <a id={matches[item]['id']} onClick={this.showResult} href="#"> Show Result</a></td>
-                     </tr>
-                    )
-                });
-        } else {
-            return  (<tr>
-                        <td>Loading...</td>
-                     </tr> );
-        }
-
-    }
-    individualValue() {
-      let matches = this.state.match;
-      if(matches.length) {
-          return Object.keys(matches)
-              .map((item, index) => {
-                  return (
-                   <tr key={index}>
-                      <td>{matches[item].de}</td>
-                   </tr>
-                  )
-              });
+        matches: [],
+        match: {}
       }
     }
-    showResult(e) {
-      e.preventDefault();
-      let match_id = e.target.id;
-      console.log(match_id);
-      fetch('https://cricscore-api.appspot.com/csa?id='+match_id)
-      .then((res) => {
-        // You were not returning the JSON to the next then()
-        return res.json()
-      })
-      .then((json) => {
-        this.setState({
-            match: json,
-            condition: 'ShowResults'
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
-    apiFetchCall(e) {
+    componentDidMount() {
       fetch('https://cricscore-api.appspot.com/csa')
-      .then((res) => {
-        return res.json()
-      })
+      .then(res => res.json())
       .then((json) => {
+        console.log(json)
         this.setState({
-            match: json
+          matches: json
         })
       })
       .catch((err) => {
         console.log(err)
       })
-
     }
     render() {
-
-
+      const { matches } = this.state
+      const data = matches.map((item, index) => {
+        return (
+          <tr key={item.id}>
+            <td>{item.t1}</td>
+            <td>{item.t2}</td>
+            <td>{item.id}</td>
+          </tr>
+        )
+      })
       return (
         <div className="App">
-        <button onClick={this.apiFetchCall}>Show Table</button>
           <table>
             <tbody>
-                    { (this.state.condition === 'ShowTable') ? (this.renderChildren()) : (this.individualValue()) }                    }
+              {data}
             </tbody>
           </table>
+
+          {
+            Object.keys(match).length !== 0 && match.constructor !== Object ?
+            <div>Render your match data here</div> :
+            null
+          }
+
         </div>
       )
     }
